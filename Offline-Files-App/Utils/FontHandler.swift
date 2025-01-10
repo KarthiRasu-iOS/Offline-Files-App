7b354814ab8802c739995b1b9adce08ec8107078
Karthi Rasu <karthirasu07@gmail.com>
2025-01-10 18:37:03 +0530

    Poppins font added and theme colors updated

---
+++ Offline-Files-App/Utils/FontHandler.swift
@@ -0,0 +1,37 @@
+//
+//  FontHandler.swift
+//  Offline-Files-App
+//
+//  Created by Karthi Rasu on 10/01/25.
+//
+
+
+import SwiftUI
+
+extension Font {
+    
+    enum Poppins {
+        case regular
+        case medium
+        case semibold
+        case bold
+        
+        var value : String {
+            switch self {
+            case .regular:
+                return "Poppins-Regular"
+            case .medium:
+                return "Poppins-Medium"
+            case .semibold:
+                return "Poppins-SemiBold"
+            case .bold:
+                return "Poppins-Bold"
+            }
+        }
+    }
+    
+    static func poppins(_ type : Poppins,size : CGFloat = 13) -> Font {
+        return .custom(type.value, size: size)
+    }
+}
+
